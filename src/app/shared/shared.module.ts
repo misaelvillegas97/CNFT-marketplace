32dbf76a2770b92a4b7d9b284d48a396f069ca59
David Misael Villegas Sandoval <david.misa97@gmail.com>
2022-01-13 00:33:46 -0300

    [SHARED MODULE] initialized shared module and material module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,1 @@
+import { NgModule }            from '@angular/core';import { CommonModule }        from '@angular/common';import { TranslateModule }     from '@ngx-translate/core';import { ReactiveFormsModule } from '@angular/forms';@NgModule({  declarations: [],  imports: [    CommonModule,    TranslateModule,    ReactiveFormsModule  ]})export class SharedModule { }
