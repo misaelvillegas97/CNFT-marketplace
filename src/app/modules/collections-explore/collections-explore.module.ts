b6a78ebcebfa6c53b8544e421a7a4775c3b4ebf4
David Misael Villegas Sandoval <david.misa97@gmail.com>
2022-01-17 01:10:48 -0300

    [COLLECTIONS-EXPLORE MODULE] - Create new module to manage all collections through categories as tabs

---
+++ src/app/modules/collections-explore/collections-explore.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollectionsExploreRoutingModule } from './collections-explore-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CollectionsExploreRoutingModule
+  ]
+})
+export class CollectionsExploreModule { }
