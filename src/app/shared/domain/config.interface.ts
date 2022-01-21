1be73037d727b3d7aaefdfe04c0a4aade592c76c
David Misael Villegas Sandoval <david.misa97@gmail.com>
2022-01-21 16:34:46 -0300

    [SHARED MODULE]
    - Added utils interfaces to shared config interface

--- src/app/shared/domain/config.interface.ts
+++ src/app/shared/domain/config.interface.ts
@@ -1,1 +1,21 @@
-import { EventEmitter } from '@angular/core';export interface IActionButton extends IButton {  code?: string;}export interface IButton {  label: string;  icon?: string;  action: ($event: any, emitter?: EventEmitter<any>) => void;  color?: MaterialColorType;  disabled?: boolean;}export type MaterialColorType = 'primary' | 'accent' | 'warn';export type textAlignType = 'left' | 'center' | 'right';
+import { EventEmitter } from '@angular/core';
+
+export interface IActionButton extends IButton {
+  code?: string;
+}
+
+export interface IButton {
+  label: string;
+  icon?: string;
+  action: ($event: any, emitter?: EventEmitter<any>) => void;
+  color?: MaterialColorType;
+  disabled?: boolean;
+}
+
+export interface IImage {
+  src: string;
+  alt?: string;
+}
+
+export type MaterialColorType = 'primary' | 'accent' | 'warn';
+export type textAlignType = 'left' | 'center' | 'right';
