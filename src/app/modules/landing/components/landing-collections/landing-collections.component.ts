04c30672f5d200e16aba3d6797e5bec6bff6c965
David Misael Villegas Sandoval <david.misa97@gmail.com>
2022-01-21 16:42:59 -0300

    [LANDING MODULE]
    - Update landing component and move to root module folder
    - Update components that use card module with new css classes written in _utils.scss

--- src/app/modules/landing/components/landing-collections/landing-collections.component.ts
+++ src/app/modules/landing/components/landing-collections/landing-collections.component.ts
@@ -1,1 +1,23 @@
-import { Component, OnInit }  from '@angular/core';import { ICardConfig }        from '@shared/components/card/domain/config.interface';import { getFakeCollections } from '@shared/utils/collections.utils';@Component({  selector: 'cf-landing-collections',  templateUrl: './landing-collections.component.html',  styleUrls: [ './landing-collections.component.scss' ]})export class LandingCollectionsComponent implements OnInit {  collections: ICardConfig[];  constructor() {    this.collections = [];  }  ngOnInit(): void {    this.collections = getFakeCollections();  }  action = ($event: any) => console.log($event);}
+import { Component, OnInit }  from '@angular/core';
+import { ICardConfig }        from '@shared/modules/card/domain/config.interface';
+import { getFakeCollectionsCardConfig } from '@shared/utils/collections.utils';
+
+@Component({
+  selector: 'cf-landing-collections',
+  templateUrl: './landing-collections.component.html',
+  styleUrls: [ './landing-collections.component.scss' ]
+})
+export class LandingCollectionsComponent implements OnInit {
+
+  collections: ICardConfig[];
+
+  constructor() {
+    this.collections = [];
+  }
+
+  ngOnInit(): void {
+    this.collections = getFakeCollectionsCardConfig();
+  }
+
+  action = ($event: any) => console.log($event);
+}
--- src/app/modules/landing/landing-routing.module.ts
+++ src/app/modules/landing/landing-routing.module.ts
@@ -1,1 +1,16 @@
-import { NgModule }             from '@angular/core';import { RouterModule, Routes } from '@angular/router';import { LandingComponent }     from './components/landing/landing.component';const routes: Routes = [  {    path: '',    component: LandingComponent,  }];@NgModule({  imports: [RouterModule.forChild(routes)],  exports: [RouterModule]})export class LandingRoutingModule { }
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent }     from './landing.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LandingRoutingModule { }
